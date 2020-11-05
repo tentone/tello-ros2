4adfd9f62f7ae5b00d4d6096336befd618a06ed7
tentone <tentone@outlook.com>
2020-11-05 15:34:34 +0000

    Base repository

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+colcon build --symlink-install
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ROS 2 Installation script for Ubuntu 20.04
+#
+# Can also be used with windows on WSL 2
+
+# Set UTF-8 charset
+apt update && apt install locales
+locale-gen en_US en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# Add ROS2 sources
+apt update && apt install curl gnupg2 lsb-release
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+# Install ROS 2 (foxy)
+apt update
+apt install ros-foxy-desktop
+
+# Step envrioment
+source /opt/ros/foxy/setup.bash
+
+# Argcomplete
+apt install -y python3-pip
+pip3 install -U argcomplete
+
+# Colcon build tools
+apt install python3-colcon-common-extensions
