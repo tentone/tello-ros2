1ae0d84b6915e5bc4e93eccb70015a4a3246e176
tentone <tentone@outlook.com>
2020-11-20 14:42:53 +0000

    Message namespace

--- workspace/src/control/src/control.cpp
+++ workspace/src/control/src/control.cpp
@@ -1,18 +1,23 @@
-#include <ros/ros.h>
 #include <signal.h>
 #include <termios.h>
 #include <stdio.h>
 #include <math.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Empty.h>
-#include <std_msgs/UInt8.h>
-#include <std_msgs/Bool.h>
-#include <geometry_msgs/Twist.h>
-#include <geometry_msgs/PointStamped.h>
-#include <geometry_msgs/Point.h>
-#include <sensor_msgs/Imu.h>
-#include <nav_msgs/Odometry.h>
-#include <tf/transform_datatypes.h>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "std_msgs/msg/String.h"
+#include "std_msgs/msg/Empty.h"
+#include "std_msgs/msg/UInt8.h"
+#include "std_msgs/msg/Bool.h"
+
+#include "geometry_msgs/msg/Twist.h"
+#include "geometry_msgs/msg/PointStamped.h"
+#include "geometry_msgs/msg/Point.h"
+
+#include "sensor_msgs/msg/Imu.h"
+#include "nav_msgs/msg/Odometry.h"
+
+#include "tf2/transform_datatypes.h"
 
 #define DEBUG true
 
@@ -71,19 +76,19 @@
  */
 #define MODE_WAYPOINT 2
 
-ros::Publisher pub_takeoff;
-ros::Publisher pub_land;
-ros::Publisher pub_velocity;
+rclcpp::Publisher pub_takeoff;
+rclcpp::Publisher pub_land;
+rclcpp::Publisher pub_velocity;
 
 /**
  * Odometry provided by the drone, used to obtain the position and rotation of the drone.
  */
-nav_msgs::Odometry odometry;
+nav_msgs::msg::Odometry odometry;
 
 /**
  * IMU data provided by the drone.
  */
-sensor_msgs::Imu imu;
+sensor_msgs::msg::Imu imu;
 
 /**
  * Mode of the drone, defines its behavior.
@@ -165,7 +170,7 @@ void manualControl(int key)
     // Only send data on changes
     if(key != manual_last_key)
     {
-        geometry_msgs::Twist msg;
+        geometry_msgs::msg::Twist msg;
 
         if(key == KEY_UP) {msg.linear.x = manual_speed;}
         if(key == KEY_DOWN) {msg.linear.x = -manual_speed;}
@@ -184,12 +189,12 @@ void manualControl(int key)
 }
 
 bool waypoint_state = WAYPOINT_WAITING;
-geometry_msgs::PointStamped waypoint;
+geometry_msgs::msg::PointStamped waypoint;
 
 /**
  * Received waypoint from external node and store it for navigation.
  */
-void waypointCallback(const geometry_msgs::PointStamped &msg)
+void waypointCallback(const geometry_msgs::msg::PointStamped &msg)
 {
     #ifdef DEBUG
         std::cout << "Received waypoint" << std::endl;
@@ -205,7 +210,7 @@ void waypointCallback(const geometry_msgs::PointStamped &msg)
 /**
  * Dot product between two vectors.
  */
-double dot(geometry_msgs::Point a, geometry_msgs::Point b)
+double dot(geometry_msgs::msg::Point a, geometry_msgs::msg::Point b)
 {
     return a.x * b.x + a.y * b.y;
 }
@@ -213,7 +218,7 @@ double dot(geometry_msgs::Point a, geometry_msgs::Point b)
 /**
  * Cross product between two vectors.
  */
-double cross(geometry_msgs::Point a, geometry_msgs::Point b)
+double cross(geometry_msgs::msg::Point a, geometry_msgs::msg::Point b)
 {
     return a.x * b.y - a.y * b.x;
 }
@@ -225,7 +230,7 @@ void waypointControl()
 {
     if(mode == MODE_WAYPOINT && (waypoint_state == WAYPOINT_HAS_WAYPOINT || waypoint_state == WAYPOINT_NAVIGATING))
     {
-        geometry_msgs::Twist msg;
+        geometry_msgs::msg::Twist msg;
 
         // Convert quaternion to euler rotation
         tf::Quaternion quaternion;
@@ -304,7 +309,7 @@ bool person_drone_moving = false;
 /**
  * Process callback contains the person velocity.
  */
-void personVelocityCallback(const geometry_msgs::Twist &msg)
+void personVelocityCallback(const geometry_msgs::msg::Twist &msg)
 {
     if(mode == MODE_FOLLOW_PERSON && person_visible)
     {
@@ -316,13 +321,13 @@ void personVelocityCallback(const geometry_msgs::Twist &msg)
 /**
  * Process callback indicating if the person if visible.
  */
-void personVisibleCallback(const std_msgs::Bool &msg)
+void personVisibleCallback(const std_msgs::msg::Bool &msg)
 {
     person_visible = msg.data;
 
     if(mode == MODE_FOLLOW_PERSON && !person_visible && person_drone_moving)
     {
-        geometry_msgs::Twist msg;
+        geometry_msgs::msg::Twist msg;
         pub_velocity.publish(msg);
         person_drone_moving = false;
     }
@@ -331,7 +336,7 @@ void personVisibleCallback(const std_msgs::Bool &msg)
 /**
  * Callback method used to read and store the odometry value.
  */
-void odomCallback(const nav_msgs::Odometry &msg)
+void odomCallback(const nav_msgs::msg::Odometry &msg)
 {
     odometry = msg;
 }
@@ -339,7 +344,7 @@ void odomCallback(const nav_msgs::Odometry &msg)
 /**
  * Callback method used to read and store the odometry value.
  */
-void imuCallback(const sensor_msgs::Imu &msg)
+void imuCallback(const sensor_msgs::msg::Imu &msg)
 {
     imu = msg;
 
@@ -371,16 +376,16 @@ void setMode(int m)
         std::cout << "Mode: " << mode << std::endl;
     #endif
 
-    geometry_msgs::Twist msg;
+    geometry_msgs::msg::Twist msg;
     pub_velocity.publish(msg);
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "control");
+    rclcpp::init(argc, argv, "control");
 
     // Node handler
-    ros::NodeHandle node;
+    rclcpp::NodeHandle node;
 
     // Subscribe topic parameters
     std::string param_sub_velocity, param_sub_visible;
@@ -395,11 +400,11 @@ int main(int argc, char **argv)
     node.param<std::string>("sub_imu", param_sub_imu, "/tello/imu");
 
     // Subscribe topic
-    ros::Subscriber sub_person_velocity = node.subscribe(param_sub_velocity, 1, personVelocityCallback);
-    ros::Subscriber sub_person_visible = node.subscribe(param_sub_visible, 1, personVisibleCallback);
-    ros::Subscriber sub_waypoint = node.subscribe(param_sub_waypoint, 1, waypointCallback);
-    ros::Subscriber sub_odom = node.subscribe(param_sub_odom, 1, odomCallback);
-    ros::Subscriber sub_imu = node.subscribe(param_sub_imu, 1, imuCallback);
+    rclcpp::Subscriber sub_person_velocity = node.subscribe(param_sub_velocity, 1, personVelocityCallback);
+    rclcpp::Subscriber sub_person_visible = node.subscribe(param_sub_visible, 1, personVisibleCallback);
+    rclcpp::Subscriber sub_waypoint = node.subscribe(param_sub_waypoint, 1, waypointCallback);
+    rclcpp::Subscriber sub_odom = node.subscribe(param_sub_odom, 1, odomCallback);
+    rclcpp::Subscriber sub_imu = node.subscribe(param_sub_imu, 1, imuCallback);
 
     // Publish topic parameters
     std::string param_pub_velocity, param_pub_takeoff, param_pub_land;
@@ -408,12 +413,12 @@ int main(int argc, char **argv)
     node.param<std::string>("pub_velocity", param_pub_velocity, "/tello/cmd_vel");
 
     // Publish topics
-    pub_takeoff = node.advertise<std_msgs::Empty>(param_pub_takeoff, 10);
-    pub_land = node.advertise<std_msgs::Empty>(param_pub_land, 10);
-    pub_velocity = node.advertise<geometry_msgs::Twist>(param_pub_velocity, 10);
+    pub_takeoff = node.advertise"std_msgs/msg::Empty>(param_pub_takeoff, 10);
+    pub_land = node.advertise"std_msgs/msg::Empty>(param_pub_land, 10);
+    pub_velocity = node.advertise<geometry_msgs::msg::Twist>(param_pub_velocity, 10);
 
     // Main loop
-    while(ros::ok())
+    while(rclcpp::ok())
     {
         int key = getch(10);
 
@@ -439,19 +444,19 @@ int main(int argc, char **argv)
         // Takeoff
         if(key == KEY_T)
         {
-            std_msgs::Empty empty;
+            std_msgs::msg::Empty empty;
             pub_takeoff.publish(empty);
         }
         // Land
         else if(key == KEY_L)
         {
-            std_msgs::Empty empty;
+            std_msgs::msg::Empty empty;
             pub_land.publish(empty);
         }
         // Home
         else if(key == KEY_H)
         {
-            geometry_msgs::PointStamped msg;
+            geometry_msgs::msg::PointStamped msg;
 
             waypoint = msg;
             waypoint_state = WAYPOINT_HAS_WAYPOINT;
@@ -467,7 +472,7 @@ int main(int argc, char **argv)
             waypointControl();
         }
 
-        ros::spinOnce();
+        rclcpp::spinOnce();
     }
 
     return 0;
