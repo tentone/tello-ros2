bc223bfe194b87bcde557e915e1ebe340cd47955
tentone <tentone@outlook.com>
2020-11-21 15:23:21 +0000

    Control package

--- install.sh
+++ install.sh
@@ -43,10 +43,12 @@ apt install python3-dev python3-numpy
 apt install libavcodec-dev libavformat-dev libswscale-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libgtk-3-dev
 apt install libpng-dev libjpeg-dev libopenexr-dev libtiff-dev libwebp-dev
 
-# Clone the 
+# Clone the repository
 cd ~
 apt-get install git
 git clone https://github.com/opencv/opencv.git
+
+# Building files
 cd opencv
 mkdir build
 cd build
