769ef840ad015e815e432e72afc6c8d707d7b4e5
José Ferrão <tentone@outlook.com>
2021-03-21 11:37:39 +0000

    Clean up install file

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,19 +7,21 @@ apt install build-essential gdb
 echo " - Installing ROS 2 Foxy"
 
 # Set UTF-8 charset
-apt update && apt install locales
+apt update
+apt install -y locales
 locale-gen en_US en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # Add ROS2 sources
-apt update && apt install curl gnupg2 lsb-release
+apt update
+apt install -y curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 
 # Install ROS 2 (foxy)
 apt update
-apt install ros-foxy-desktop
+apt install -y ros-foxy-desktop
 
 # Step envrioment
 source /opt/ros/foxy/setup.bash
@@ -29,8 +31,8 @@ apt install -y python3-pip
 pip3 install -U argcomplete
 
 # Colcon build tools
-apt install python3-colcon-common-extensions
-apt install python3-rosdep2
+apt install -y python3-colcon-common-extensions
+apt install -y python3-rosdep2
 
 # Update ROS dep
 rosdep update
@@ -38,14 +40,14 @@ rosdep update
 echo " - Installing OpenCV"
 
 # Install OpenCV from source
-apt install cmake gcc g++
-apt install python3-dev python3-numpy
-apt install libavcodec-dev libavformat-dev libswscale-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libgtk-3-dev
-apt install libpng-dev libjpeg-dev libopenexr-dev libtiff-dev libwebp-dev
+apt install -y cmake gcc g++
+apt install -y python3-dev python3-numpy
+apt install -y libavcodec-dev libavformat-dev libswscale-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libgtk-3-dev
+apt install -y libpng-dev libjpeg-dev libopenexr-dev libtiff-dev libwebp-dev
 
 # Clone the repository
 cd ~
-apt-get install git
+apt install -y git
 git clone https://github.com/opencv/opencv.git
 
 # Building files
