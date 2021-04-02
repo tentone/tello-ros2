0d5ef0e05e751aba9f8259f67da8f1d0ca326259
José Ferrão <tentone@outlook.com>
2021-04-02 11:07:34 +0100

    Launch file

---
+++ workspace/src/launch.py
@@ -0,0 +1,19 @@
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='control',
+            executable='control',
+            namespace='/',
+            name='control'
+        ),
+        # Node(
+        #     package='subscriber',
+        #     executable='subscriber',
+        #     namespace='/',
+        #     name='subscriber'
+        # )
+    ])
