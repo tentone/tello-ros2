fad2ff57e5cfd63dc1b8cb0d182eecf84b55bb78
José Ferrão <tentone@outlook.com>
2021-04-03 18:39:41 +0100

    Gitignore

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,6 +11,8 @@ apt install ros-foxy-ament-cmake* ros-foxy-tf2* ros-foxy-rclcpp*
 
 echo " - Tello ROS Node"
 apt install libasio-dev ros-foxy-cv-bridge ros-foxy-camera-calibration-parsers
+pip3 install empy
+
 mkdir ../workspace/src/lib
 cd ../workspace/src/lib
 git clone https://github.com/clydemcqueen/tello_ros.git
