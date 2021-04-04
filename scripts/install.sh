12998f0eb95432b6aae1bc3c85690d23e1e77ae5
José Ferrão <tentone@outlook.com>
2021-04-04 11:56:40 +0100

    Missing dep

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,14 +9,4 @@ pip3 install catkin_pkg rospkg rosdep2 av
 echo " - CPP dependencies"
 apt install ros-foxy-ament-cmake* ros-foxy-tf2* ros-foxy-rclcpp*
 
-echo " - Tello ROS Node"
-apt install libasio-dev ros-foxy-cv-bridge ros-foxy-camera-calibration-parsers
-pip3 install empy
-
-mkdir ../workspace/src/lib
-cd ../workspace/src/lib
-git clone https://github.com/clydemcqueen/tello_ros.git
-git clone https://github.com/ptrmu/ros2_shared.git
-
-source /opt/ros/foxy/setup.bash
-colcon build --event-handlers console_direct+ --packages-skip tello_gazebo
+./install-libs.sh
