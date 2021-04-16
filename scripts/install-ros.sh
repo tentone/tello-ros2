44297eca1afbc17461e939f31e426404af2ddae2
tentone <tentone@outlook.com>
2021-04-16 23:28:31 +0100

    Scripts

---
+++ scripts/install-ros.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+echo " - Installing ROS 1 Noetic"
+
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+apt update
+apt install build-essential gdb ros-noetic-desktop-full
+
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+echo " - Python dependencies"
+
+apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+rosdep init
+rosdep update
+
+
+echo " - ROS 1 to ROS2 bridge"
+
+apt install -y ros-dashing-rosbag2-bag-v2-plugins
--- scripts/install-ros2.sh
+++ scripts/install-ros2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+#Install ROS 2
+echo " - Installing ROS 2 Foxy"
+
 echo " - Install Build Tools"
 
 # C++ Build tools
 apt install build-essential gdb
-
-#Install ROS 2
-echo " - Installing ROS 2 Foxy"
 
 # Set UTF-8 charset
 apt update
@@ -15,7 +15,7 @@ locale-gen en_US en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-echo " - ROS2 sources"
+echo " - ROS 2 sources"
 
 # Add ROS2 sources
 apt update
@@ -23,7 +23,7 @@ apt install -y curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 
-echo " - Install ROS2"
+echo " - Install ROS 2"
 
 # Install ROS 2 (foxy)
 apt update
@@ -32,7 +32,7 @@ apt install -y ros-foxy-desktop
 # Step envrioment
 source /opt/ros/foxy/setup.bash
 
-echo " - Install Python ROS2"
+echo " - Install Python ROS 2"
 
 # Argcomplete
 apt install -y python3-pip
@@ -45,24 +45,9 @@ apt install -y python3-colcon-common-extensions python3-rosdep2
 rosdep update
 rosdep fix-permissions
 
-# Install OpenCV from source
-echo " - Installing OpenCV"
-apt install -y cmake gcc g++ python3-dev python3-numpy libavcodec-dev libavformat-dev libswscale-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libgtk-3-dev libpng-dev libjpeg-dev libopenexr-dev libtiff-dev libwebp-dev
-
-# Clone the repository
-cd ~
-apt install -y git
-git clone https://github.com/opencv/opencv.git
-
-# Building files
-cd opencv
-mkdir build
-cd build
-cmake ..
-make install -j4
 
 # Add to bashrc
-echo " - Register ROS2 in .bashrc"
+echo " - Register ROS 2 in .bashrc"
 echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
 echo "export _colcon_cd_root=~/ros2_install" >> ~/.bashrc
