a255aaa4443fe1ee3948d00ac2f7bdd3a0d51241
tentone <tentone@outlook.com>
2021-05-12 22:25:05 +0100

    Pangolin setup

--- scripts/orbslam.sh
+++ scripts/orbslam.sh
@@ -12,6 +12,15 @@ echo "- Create libs folder"
 mkdir -p ../libs
 cd ../libs
 
+echo " - Install Pangolin"
+apt install libgl1-mesa-dev libglew-dev cmake libegl1-mesa-dev libwayland-dev libxkbcommon-dev wayland-protocols ffmpeg libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libavdevice-dev libjpeg-dev libpng-dev libtiff5-dev libopenexr-dev
+git clone https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin
+mkdir build
+cd build
+cmake ..
+make install -j8
+
 echo " - Install G2O"
 apt install cmake libeigen3-dev libsuitesparse-dev qtdeclarative5-dev qt5-qmake libqglviewer-dev-qt5
 git clone https://github.com/RainerKuemmerle/g2o.git
@@ -19,11 +28,12 @@ cd g2o
 mkdir build
 cd build
 cmake ..
-make install
+make install -j8
 cd ../../libs
 
 echo " - Download ORB SLAM 2"
-
 mkdir -p ../workspace/src/libs
 cd ../workspace/src/libs
-git clone https://github.com/alsora/ORB_SLAM2
+# git clone https://github.com/alsora/ORB_SLAM2
+git clone https://github.com/alsora/ros2-ORB_SLAM2
+
